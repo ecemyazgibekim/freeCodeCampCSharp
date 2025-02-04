c98f40e6d39e4bc0be7b4bcc52fcd5c421d80ee0
Ecem Yazgi Bekim <ecemyazgi.bekim@bakircay.edu.tr>
2025-02-04 08:31:29 +0300

    Arrays Basic

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luckyNumbers = { 4, 8, 14, 60, 99 };
+            string[] friends = new string[5];
+            friends[0] = "Jim";
+            friends[1] = "Kelly";
+
+            Console.WriteLine(luckyNumbers[1]);
+            Console.Read();
+        }
+    }
+}
