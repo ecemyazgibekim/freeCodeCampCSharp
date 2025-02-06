e0f9b23e89cedd0502067f9fa0b38b4254c3a3c6
Ecem Yazgi Bekim <ecemyazgi.bekim@bakircay.edu.tr>
2025-02-06 22:30:17 +0300

    StaticMethodsAndClasses
    Static bir method olan Math ile kendi oluşturduğumuz sınıfa static yazıp yazmadığımız durumlarda nesne yaratıp yaratamama üzerine bir çalışma. Static yazdığımızda nesne yaratamıyoruz.

---
+++ StaticMethodsAndClasses/StaticMethodsAndClasses/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMethodsAndClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Math myMath=new Math(); diyemezsin!! hata verir çünkü static bir method nesne üretemezsin
+            UsefulTools tools = new UsefulTools(); //diyebiliriz static method değil
+            UsefulTools.SayHi("Mike");
+            Console.WriteLine(Math.Sqrt(144));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StaticMethodsAndClasses/StaticMethodsAndClasses/UsefulTools.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMethodsAndClasses
+{
+    class UsefulTools //eğer static yaparsam bundan bir nesne yaratamayız!!
+    {
+        public static void SayHi(string name)
+        {
+            Console.WriteLine("Hello "+name);
+        }
+    }
+}
