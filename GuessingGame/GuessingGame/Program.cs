f75396014f2e08fe08ae7bb118284c75f6f04e12
Ecem Yazgi Bekim <ecemyazgi.bekim@bakircay.edu.tr>
2025-02-06 13:49:40 +0300

    Guessing Game
    "giraffe" kelimesini tahmin ettirme oyunudur. Kullanıcı kelimeyi tahmin etmeye çalışır.

---
+++ GuessingGame/GuessingGame/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "giraffe";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false; //kullanıcının tahminlerinin tükenip tükenmediğini söyler
+
+            
+            while (guess != secretWord & !outOfGuesses)
+            {
+                if (guessCount < guessLimit) { 
+                    Console.Write("Enter guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                    }
+                else
+                {
+                    outOfGuesses=true;
+                }
+            }
+            if (outOfGuesses)
+            {
+                Console.Write("You lose!");
+            }
+            else
+            {
+                Console.Write("You win!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
