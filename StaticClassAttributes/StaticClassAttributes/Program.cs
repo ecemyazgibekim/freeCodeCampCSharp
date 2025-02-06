782fc91e634dde16b00c35c579ae7c47624ea4ff
Ecem Yazgi Bekim <ecemyazgi.bekim@bakircay.edu.tr>
2025-02-06 22:22:12 +0300

    Static Class Attributes
    Bir static değişken oluşturmaya dair bilgiler

---
+++ StaticClassAttributes/StaticClassAttributes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClassAttributes
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Song holiday = new Song("Holiday", "Green Day", 200);
+            Console.WriteLine(Song.songCount);
+            Song kashmir = new Song("Kashmir", "Led Zeppelin", 150);
+            Console.WriteLine(kashmir.getSongCount()); //sınıfa ait olduğu için nesne oluşturmadan direkt çağrılabiliyor
+
+            Console.WriteLine(holiday.artist); //title her nesneye özel ama static songcount ise sınıa özel
+            Console.WriteLine(kashmir.artist); 
+            Console.ReadLine();
+
+
+            //---ÖZETLE
+            /* 
+             
+             *  Static değişken (songCount) → Tüm nesneler için ortak, class'a ait.
+                Normal değişken (title, artist) → Her nesneye özel, instance'a ait.
+                Static metodlar (GetSongCount()) → Nesne oluşturmadan doğrudan sınıftan çağrılır (Song.GetSongCount()).
+                Kod daha iyi yazılmalıydı çünkü songCount zaten static olduğu için getSongCount() da static olmalıydı.
+
+                Eğer bir değişken her nesne için farklı olmalıysa → Normal (non-static) değişken kullan.
+                Eğer bir değişken tüm nesneler için ortak olmalıysa → static kullan.
+             */
+
+        }
+    }
+}
---
+++ StaticClassAttributes/StaticClassAttributes/Song.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClassAttributes
+{
+     class Song
+    {
+        public string title;
+        public string artist;
+        public int duration;
+        public static int songCount=0;
+
+        //static anahtar kelimesi, bir değişkenin veya metodun tüm sınıfa (class) ait olmasını sağlar.
+        //static olduğu için song classa ait bir değişken,her song nesnesi oluşturulunca da soungcount bir artırılacak
+        public Song(string aTitle, string aArtist, int aDuration)
+        {
+            title = aTitle;
+            artist = aArtist;
+            duration = aDuration;
+            songCount++;
+        }
+
+        public int getSongCount()
+        {
+            return songCount;
+        }
+    }
+}
