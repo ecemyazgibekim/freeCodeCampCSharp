23dcfc1cea4a9a4f138425857870a3ea4349bc6b
Ecem Yazgi Bekim <ecemyazgi.bekim@bakircay.edu.tr>
2025-02-06 16:58:27 +0300

    ClassesAndObjects
    Instance ve attribute olaylarının temel olarak ne olduğu açıklandı.

---
+++ ClassesAndObjects/ClassesAndObjects/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+    internal class Book
+    {
+        //kitabın ne olduğunu tanımlayacak nitelikler(attributes) yazacağız
+        public string title;
+        public string author;
+        public int pages;
+    }
+}
---
+++ ClassesAndObjects/ClassesAndObjects/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //gerçek dünyadaki nesneleri düşün bir telefonu int bool ile ifade edemeyiz daha karmaşık
+            //nesneler kendi özel veri tiplerimizi oluşturmamızı sağlar(objects)
+            // sınıf ile gerçek dünyadakileri modelleyebiliriz. Karmaşık bir veri türü yaratabiliriz mesela book sınıfı ile
+
+            Book book1=new Book(); //book classının bir instanceı(örneğini) yarattık. nesne yarattım yani
+            book1.title = "Harry Potter";
+            book1.author = "JK  Rowling";
+            book1.pages = 400;
+
+
+            Book book2= new Book(); 
+            book2.title = "LOTR";
+            book2.author = "JRR Tolkien";
+            book2.pages = 700;
+
+
+            Console.WriteLine("Book 1 Title:" + book1.title);
+            Console.WriteLine("Book 2 Author:" + book2.author);
+
+        }
+    }
+}
