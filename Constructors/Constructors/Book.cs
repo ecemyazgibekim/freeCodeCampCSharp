5acb367aa838427b6fba61aef02a28d2b07622b0
Ecem Yazgi Bekim <ecemyazgi.bekim@bakircay.edu.tr>
2025-02-06 17:21:47 +0300

    Constructor
    Overwrite nedir onu da içeriyor.

---
+++ Constructors/Constructors/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class Book
+    {
+
+        public string title;
+        public string author;
+        public int pages;
+
+        //constructor(yapıcı metot) bir classtan object oluşturduğumuzda ilk çalışacak olan özel bir metot
+        //nesneye başlangıç değeri vermek, kod tekrarını önlemek ve oluşturulan nesneyi kullanıma hazır hale getirmek  için kullanıyoruz
+        // constr olmazsa elle değer atamak zorunda kalırız
+        public Book()
+        {
+            //aynı isimli başka bir constr bunun parametresi yok
+        }
+
+        public Book(string aTitle, string aAuthor, int aPages)
+        {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+        }
+    }
+}
---
+++ Constructors/Constructors/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Book book1=new Book();
+           Book book2=new Book("Harry Potter","JK Rowling", 400);
+           Book book3 = new Book("LOTR", "JRR Tolkien", 700);
+
+           Console.WriteLine(book2.title);
+            //Overwrite işlemi ,  deconstructor C++ ama C# da doğrudan öyle geçmiyo ezme işlemi yaptık  overwrite etmek
+            book3.title = "Hobbit";
+            Console.WriteLine(book3.title);
+           Console.ReadLine();
+        }
+    }
+}
