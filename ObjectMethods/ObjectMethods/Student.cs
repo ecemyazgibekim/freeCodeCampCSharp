60a23fbba806d25b8c3c045a8ae62e9ae8339525
Ecem Yazgi Bekim <ecemyazgi.bekim@bakircay.edu.tr>
2025-02-06 18:01:20 +0300

    Object Methods

---
+++ ObjectMethods/ObjectMethods/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectMethods
+{
+    public class Student
+    {
+        public string name;
+        public string major;
+        public double gpa;
+
+        public Student(string aName, string aMajor, double aGpa)
+        {
+            name = aName;
+            major = aMajor;
+            gpa = aGpa;
+        }
+
+        public bool HasHonors()
+        {
+            //gpa=90.4 gibi de yapabilirim
+            if (gpa >= 3.5)  // gpayı kolayca değiştirebilirim sınırları kolayca değiştirebiliyorum kolaylık sağlayacak
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+}
